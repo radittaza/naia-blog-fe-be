86f89961a0da9e3992614daa3feecf94d515fe67
radittaza <radityaazanata028@gmail.com>
2026-07-28 07:44:06 +0800

    style: Navbar & Navlink

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -33,32 +33,41 @@
                         <!-- Profile dropdown -->
                         <el-dropdown class="relative ml-3">
                             @if (Auth::check())
-                                
-                                
-                            <button
-                            class="relative flex max-w-xs items-center rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 cursor-pointer">
-                                <span class="absolute -inset-1.5"></span>
-                                <span class="sr-only">Open user menu</span>
-                                <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                    alt=""
-                                    class="size-8 rounded-full outline -outline-offset-1 outline-white/10" />
-                                <div class="text-white ml-3 font-medium">{{ Auth::user()->name }}</div>
-                            </button>
+                                <button
+                                    class="relative flex max-w-xs items-center rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 cursor-pointer">
+                                    <span class="absolute -inset-1.5"></span>
+                                    <span class="sr-only">Open user menu</span>
+                                    <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                        alt=""
+                                        class="size-8 rounded-full outline -outline-offset-1 outline-white/10" />
+                                    <div class="text-gray-300 ml-3 font-medium">{{ Auth::user()->name }}</div>
+                                    <svg class="fill-current h-4 w-4 text-gray-300" xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 20 20">
+                                        <path fill-rule="evenodd"
+                                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                            clip-rule="evenodd" />
+                                    </svg>
+                                </button>
 
-                            <el-menu anchor="bottom end" popover
-                                class="w-48 origin-top-right rounded-md bg-gray-800 py-1 outline-1 -outline-offset-1 outline-white/10 transition transition-discrete [--anchor-gap:--spacing(2)] data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in">
-                                <a href="#"
-                                    class="block px-4 py-2 text-sm text-gray-300 focus:bg-white/5 focus:outline-hidden">Your
-                                    profile</a>
-                                <a href="#"
-                                    class="block px-4 py-2 text-sm text-gray-300 focus:bg-white/5 focus:outline-hidden">Settings</a>
-                                <a href="#"
-                                    class="block px-4 py-2 text-sm text-gray-300 focus:bg-white/5 focus:outline-hidden">Sign
-                                    out</a>
-                            </el-menu>
+                                <el-menu anchor="bottom end" popover
+                                    class="w-48 origin-top-right rounded-md bg-gray-800 py-1 outline-1 -outline-offset-1 outline-white/10 transition transition-discrete [--anchor-gap:--spacing(2)] data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in">
+                                    <a href="/profile"
+                                        class="block px-4 py-2 text-sm text-gray-300 focus:bg-white/5 focus:outline-hidden">Your
+                                        profile</a>
+                                    <a href="/dashboard"
+                                        class="block px-4 py-2 text-sm text-gray-300 focus:bg-white/5 focus:outline-hidden">Settings</a>
+                                    <form method="POST" action="/logout">
+                                        @csrf
+                                        <button
+                                            class="block w-full text-left px-4 py-2 cursor-pointer text-sm text-gray-300 focus:bg-white/5 focus:outline-hidden">Log
+                                            out</button>
+                                    </form>
+                                </el-menu>
                             @else
-                            <a href="/register" class="hover:text-gray-600 text-sm text-white font-medium">Register</a> <span class="text-white font-normal">|</span>
-                            <a href="/login" class="hover:text-gray-600 text-sm font-medium text-white">Login</a>
+                                <a href="/register"
+                                    class="hover:text-gray-600 text-sm text-white font-medium">Register</a> <span
+                                    class="text-white font-normal">|</span>
+                                <a href="/login" class="hover:text-gray-600 text-sm font-medium text-white">Login</a>
                             @endif
 
                         </el-dropdown>
@@ -92,37 +101,55 @@
                 <x-my-nav-link class="block" href="/contact" :current="request()->is('contact')">Contact</x-my-nav-link>
             </div>
             <div class="border-t border-white/10 pt-4 pb-3">
-                <div class="flex items-center px-5">
-                    <div class="shrink-0">
-                        <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                            alt="" class="size-10 rounded-full outline -outline-offset-1 outline-white/10" />
+                @if (Auth::check())
+                    {{-- pembungkus1 --}}
+                    <div class="flex items-center px-5">
+                        <div class="shrink-0">
+                            <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                alt="{{ Auth::user()->name }}"
+                                class="size-10 rounded-full outline -outline-offset-1 outline-white/10" />
+                        </div>
+                        <div class="ml-3">
+                            <div class="text-base/5 font-medium text-white">{{ Auth::user()->name }}</div>
+                        </div>
+                        <button type="button"
+                            class="relative ml-auto shrink-0 rounded-full p-1 text-gray-400 hover:text-white focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500">
+                            <span class="absolute -inset-1.5"></span>
+                            <span class="sr-only">View notifications</span>
+                            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5"
+                                data-slot="icon" aria-hidden="true" class="size-6">
+                                <path
+                                    d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
+                                    stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                        </button>
                     </div>
-                    <div class="ml-3">
-                        <div class="text-base/5 font-medium text-white">Tom Cook</div>
-                        <div class="text-sm font-medium text-gray-400">tom@example.com</div>
+
+                    {{-- pembungkus 2 --}}
+                    <div class="mt-3 space-y-1 px-2">
+                        <a href="/profile"
+                            class="block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-white/5 hover:text-white">Your
+                            profile</a>
+                        <a href="/dashboard"
+                            class="block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-white/5 hover:text-white">Settings</a>
+                        <form action="/logout" method="POST">
+                        @csrf
+                        <button
+                            class="block w-full text-left rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-white/5 hover:text-white">Log
+                            out</button>
+                        </form>
                     </div>
-                    <button type="button"
-                        class="relative ml-auto shrink-0 rounded-full p-1 text-gray-400 hover:text-white focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500">
-                        <span class="absolute -inset-1.5"></span>
-                        <span class="sr-only">View notifications</span>
-                        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5"
-                            data-slot="icon" aria-hidden="true" class="size-6">
-                            <path
-                                d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
-                                stroke-linecap="round" stroke-linejoin="round" />
-                        </svg>
-                    </button>
-                </div>
-                <div class="mt-3 space-y-1 px-2">
-                    <a href="#"
-                        class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-white/5 hover:text-white">Your
-                        profile</a>
-                    <a href="#"
-                        class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-white/5 hover:text-white">Settings</a>
-                    <a href="#"
-                        class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-white/5 hover:text-white">Sign
-                        out</a>
-                </div>
+                @else
+                    <div class="mt-3 space-y-1 px-2">
+                        <a href="/login"
+                            class="block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-white/5 hover:text-white">
+                            Login</a>
+                        <a href="/register"
+                            class="block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-white/5 hover:text-white">
+                            Register</a>
+                    </div>
+                @endif
+
             </div>
         </el-disclosure>
     </nav>
